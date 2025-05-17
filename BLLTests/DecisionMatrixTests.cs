83a2d4e904e204291d55e94a4439f0a5304e7913
galacticaraor <apfan@DESKTOP-NGNRCN7>
2025-05-17 14:49:02 -0500

    Added a unit test

---
+++ BLLTests/DecisionMatrixTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    [TestClass()]
+    public class DecisionMatrixTests
+    {
+        [TestMethod()]
+        public void AddPro_Nominal_Added()
+        {
+
+        }
+    }
+}
---
+++ UnitTests/DecisionMatrixTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    [TestClass()]
+    public class DecisionMatrixTests
+    {
+        [TestMethod()]
+        public void AddPro_Nominal_Added()
+        {
+            //Arrange
+            DecisionMatrix dm = new DecisionMatrix();
+            //Act
+            dm.AddPro(new Factor("UUID", "Name1", 1));
+            dm.AddPro(new Factor("UUID", "Name1", 2));
+            //Assert
+            Assert.AreEqual(3, dm.ProWeight);
+        }
+    }
+}
